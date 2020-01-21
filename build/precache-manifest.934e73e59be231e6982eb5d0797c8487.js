d700539dbc3267db68124de1598fe39058eb1a65
Claire <smith.clairelouise@gmail.com>
2020-01-21 11:02:28 +0000

    ready with favicon

---
+++ build/precache-manifest.934e73e59be231e6982eb5d0797c8487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f05ebc56ebd0f4dad9822feb530721f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f864b9c0dd05495be3c3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0c0cdfacbd181d2a8c99",
+    "url": "/static/js/2.79a6036f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.79a6036f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f864b9c0dd05495be3c3",
+    "url": "/static/js/main.e46c3806.chunk.js"
+  },
+  {
+    "revision": "475525e6fdf690329075",
+    "url": "/static/js/runtime-main.d75af611.js"
+  }
+]);
